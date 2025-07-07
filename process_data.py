81f3225b426d29bc237c00257177a48f8d3be9b4
Spencer Weishaar <sbweishaar@icloud.com>
2025-07-06 22:45:14 -0500

    Just finished up scale_data function in process_data.py. I literally just finished writing the function, so I will have to make sure it is functional and saves the data

--- process_data.py
+++ process_data.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import pearsonr
 from datetime import datetime
+from sklearn.preprocessing import StandardScaler
+
+
 
 
 
@@ -22,45 +25,54 @@ class DataProcessor:
         pass
 
     def import_data(self):
-        file_path = f"selenium_data/biweekly/player_data_2024-25_biweekly.csv"
-
-        temp_dict = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0}
-
-
-        total_stats = []
-        with open(file_path, 'r') as file:
-            reader = csv.reader(file)
-            reader.__next__()  # Skip the header row
-            for row in reader:
-                new_row = [row[0], ast.literal_eval(row[1])]
-                # print(f"new_row = {new_row}")
-                # return
-                length = len(new_row[1][0]) # Finds the length of the list, not the identifier, in the new row list
-
-                print(f"new_row = {new_row[1]}, length = {length}")
-
-                if length > 14:
-                    print(f"Player with more than 14 time-splits: {new_row[0]} with {length} time-splits")
-                # FIGURE OUT THIS LOGIC
-                '''if length not in temp_dict:
-                    temp_dict[length] = 0'''
-                
-                for key, value in temp_dict.items():
-                    if length >= int(key):
-                        temp_dict[key] += 1
-                        
-                # temp_dict[length] += 1
-
-                total_stats.append(new_row)
-            
-        print(f"Total temp_dict: {temp_dict}")
-        for key, value in temp_dict.items():
-            print(f"Number of players with at least {key} time-splits: {value}")
         
+        list_of_yearly_data = []
+        complete_players = []
+
+        for i in range(20, 25):
             
+            year_string = f"20{i}-{i+1}"
+
+            file_path = f"selenium_data/biweekly/player_data_20{i}-{i+1}_biweekly.csv"
+
+            temp_dict = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0}
+
+            total_yearly_stats = []
+            
+            with open(file_path, 'r') as file:
+                reader = csv.reader(file)
+                reader.__next__()  # Skip the header row
+                for row in reader:
+                    new_row = [row[0], ast.literal_eval(row[1]), year_string]
+                    length = len(new_row[1][0]) # Finds the length of the list, not the identifier, in the new row list
+
+                    # print(f"new_row = {new_row[1]}, length = {length}")
+
+                    if length == 10:
+                        # Add player to the list of complete players
+                        complete_players.append(new_row)
+
+                    # FIGURE OUT THIS LOGIC
+                    '''if length not in temp_dict:
+                        temp_dict[length] = 0'''
+                    
+                    for key, value in temp_dict.items():
+                        if length >= int(key):
+                            temp_dict[key] += 1
+                            
+                    # temp_dict[length] += 1
+
+                    total_yearly_stats.append(new_row)
+                
+            print(f"Year: 20{i}-{i+1}")
+            for key, value in temp_dict.items():
+                print(f"Number of players with at least {key} time-splits: {value}")
+
+            list_of_yearly_data.append(total_yearly_stats)
+
+        return list_of_yearly_data, complete_players
 
 
-        return total_stats
 
     
     def parse_start_date(self, range_str):
@@ -75,17 +87,19 @@ class DataProcessor:
 
         
 
-    def visualize_data(self, total_stats):
+    def visualize_data(self, total_yearly_stats, complete_players):
         # Define players of interest
         player_names = [
         #"Jayden Dawson", 
         #"Tre White", 
         "Dajuan Harris",
-        "Flory Bidunga",
+        # "Flory Bidunga",
         "KJ Adams",
+        "Hunter Dickinson",
+        "Zeke Mayo",
         # "Melvin Council",
         # "Cooper Flagg",
-        "Bennett Stirtz",
+        # "Bennett Stirtz",
         ]
         colors = ['darkorange', 'blue', 'green', 'red', 'purple', 'cyan', 'magenta', 'yellow']
 
@@ -93,7 +107,7 @@ class DataProcessor:
         player_data = {}
         for name in player_names:
             
-            matching = [row for row in total_stats if row[0] == name]
+            matching = [row for row in total_yearly_stats if row[0] == name]
             if not matching:
                 print(f"No data found for player: {name}")
                 return
@@ -140,19 +154,110 @@ class DataProcessor:
         plt.grid(True, linestyle='--', alpha=0.7)
         plt.legend()
         plt.tight_layout()
-        plt.show()
+        # plt.show()
 
         return
+    
+    
+
+    def scale_data(self, complete_players):
+
+        # Create a list with the same format as complete_players, but without the player identifiers
+        scaled_data = []
+        scaler_list = []
+
+        # player_data example:
+        # ['player name', [   [  [[time_split data1, time_split data2, ...], 'time_split_string'], ...    ], "player_identifier"]]
+
+        # These variables below are used to find the length of different lists in complete_players, to be used in loops below
+        first_player = complete_players[0]
+        first_player_stats = first_player[1]
+        first_player_time_splits = first_player_stats[0] # strips the player identifier
+        first_player_first_time_split_list_with_timesplit_string = first_player_time_splits[0]  # This is the first player's first time split data list, which will be used the loop below
+        first_player_first_time_split_list = first_player_first_time_split_list_with_timesplit_string[0]  # This is the first player's first time split data list without the time split string
+        # Above variable is being used in the loop below to determine the number of stats to scale
+        print(f"first_player_first_time_split_list: {first_player_first_time_split_list}")
+
+ 
+
+
+
+        # This loop gets all the stats for one stat catagory to find the scaler of the stat
+        for i in range (len(first_player_first_time_split_list)): # Use the length of the first player's first time split data list to determine the number of stats
+            # get a stat from all players and scale it
+            scaler = StandardScaler()
+            unscaled_stat_list = []
+            for player_data in complete_players:
+
+                for time_split_list in player_data[1][0]:  # This gets the time split data list for each player
+                    # time_split_list = [[time_split data1, time_split data2, ...], 'time_split_string']
+                    '''print(f"time_split_list: {time_split_list}")
+                    print(f"time_split_list[i] type: {type(time_split_list[0])}")
+                    print(f"player_data[1][0]: {player_data[1][0]}")'''
+                    # return
+                    temp_stat_value = float(time_split_list[0][i])  # This gets the i-th stat from each player's time split data list
+                    unscaled_stat_list.append(temp_stat_value)  # This gets the i-th stat from each player's time split data list
+            
+            numpy_stats = np.array(unscaled_stat_list).reshape(-1, 1)  # Reshape for scaling
+            scaled_stat_list = scaler.fit_transform(numpy_stats)  # Scale the stats
+            scaler_list.append(scaler)  # Store the scaler for later use
+            # scaled_data.append([first_player[0], [scaled_stat_list]])
+
+
+
+
+
+
+
+
+
+        # This loop scales the stats for each player using the scalers created above
+        for player_data in complete_players:
+
+            scaled_player_stats = [player_data[0], [[],[],[],[],[],[],[],[],[],[]], player_data[2]]  # This is the new scaled list of the player's stats
+
+            # This loop loops through all of the time splits for a player
+            for k in range(len(first_player_time_splits)):
+                time_split_list = player_data[1][0][k]  # This gets the time split data list for the player (includes time split string)
+                # print(f"time_split_list: {time_split_list}")
+                # return
+                
+                # This loop loops through all the stats in a time split for a player
+                for i in range(len(first_player_first_time_split_list)):
+                    unscaled_stat = float(time_split_list[0][i])
+                    scaled_stat = scaler_list[i].transform([[unscaled_stat]])
+                    scaled_player_stats[1][k].append(scaled_stat)
+
+            print(f"scaled_player_stats: {scaled_player_stats}")
+            return
+
+            scaled_data.append(scaled_player_stats)
+
+            '''print(f"player_data length: {len(player_data[1][0])}")
+            print(f"player_data: {player_data[1][0]}")'''
+
+            scaled_data.append(scaled_player_stats)  # Append the scaled player stats to the scaled_data list
+            return
+        
+        return scaled_data
+    
+        
 
 
 def main():
     processor = DataProcessor()
-    data = processor.import_data()
+    yearly_data, complete_players = processor.import_data()
 
-    processor.visualize_data(data)
+    print(f"type of data: {type(yearly_data)}")
+    print(f"first entry in complete_players: {complete_players[0]}")
+    print(f"length of complete_players: {len(complete_players)}")
+
+    processor.visualize_data(yearly_data[-1], complete_players)  # Pass in the last year in data
 
     '''for row in data:
         print(f"Player: {row[0]}, stats = {row[1]}")'''
+    
+    scaled_data = processor.scale_data(complete_players)
 
 
 
@@ -160,7 +265,13 @@ if __name__ == "__main__":
     main()
 
 
-
+# Getting the data to work with missing time splits is a challenge. There are a few options to handle this:
 # Option 1: Only train model on players with correct amount of time splits. 
 # Option 2: Replace missing time splits with the average of the previous and next time split.
 # Option 3: Use a more complex model that can handle missing data.
+
+
+# Next Steps:
+# 1. Transform complete player data into a format suitable for training.
+# 1a. Create a function that finds the length of rise or fall from split to split for each player (like the stock market video). 
+# 2. Save and export to model file. 
